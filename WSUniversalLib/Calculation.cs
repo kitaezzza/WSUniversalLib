eab0f89600aba7af5990e97a4a597f93318206c8
stud <stud@PK511-4>
2023-05-15 11:24:32 +0400

    Добавьте файлы проекта.

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,70 @@
+﻿namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        /// <summary>
+        /// метод расчитывает целое кол-во сырья, необходимое для производства определенного кол-ва продукции, учитывая возможный брак материалов
+        /// </summary>
+        /// <param name="productType"> Продукция</param>
+        /// <param name="materialType"> Материал</param>
+        /// <param name="count"> Кол-во </param>
+        /// <param name="width"> Ширина</param>
+        /// <param name="length"> Длина</param>
+        /// <returns> Целое кол-во сырья, (-1) если не верные параметры</returns>
+
+
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            if (count <= 0 || width <= 0 || length <= 0)
+                return -1;
+            try {
+                float coefproductType = GetProductType(productType);
+                float coefMaterialType = GetMaterialType(materialType);
+                float notDefect = GetMaterialNotDefect(coefproductType, count, width, length);
+                float defect = GetDefect(notDefect, coefMaterialType);
+                return RoundUp(notDefect + defect); // заглушка
+            }
+            catch (Exception ex)
+                {
+                   return -1;
+                }
+            }
+
+        private int RoundUp(float itog)
+        {
+         return (int) Math.Ceiling(itog);
+        }
+
+        private float GetDefect(float notDefect, float coefMaterialType)
+        {
+         return notDefect* coefMaterialType;
+        }
+
+        private float GetMaterialNotDefect(float coefproductType, int count, float width, float length)
+        {
+          return (float)Math.Ceiling(coefproductType * width * length * count);
+        }
+
+       
+
+        private float GetMaterialType(int materialType)
+        {
+            switch (materialType)
+            {
+             case 1: return 0.003f;
+             case 2: return 0.0012f;
+             default: throw new Exception();
+            }
+        }
+        private float GetProductType(int productType)
+            {
+               switch(productType)
+                {
+                case 1: return 1.1f;
+                case 2: return 2.5f;
+                case 3: return 8.43f;
+                default: throw new Exception();
+               }
+            }
+    }
+}
---
+++ WSUniversalLibTests/CalculationTests.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void GetQuantityForProductTest_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float w = 20f;
+            float h = 45f;
+            int expected = 114148;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest4_Minus_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float w = -20f;
+            float h = 45f;
+            int expected = -1;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest2_Minus_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float w = -20f;
+            float h = 45f;
+            int expected = -1;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest3_Minus_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float w = 20f;
+            float h = -45f;
+            int expected = -1;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest5_Minus_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = -15;
+            float w = 20f;
+            float h = 45f;
+            int expected = -1;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest7_Minus_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = -1;
+            int count = 15;
+            float w = 20f;
+            float h = 45f;
+            int expected = -1;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest6_Minus_1()
+        {
+
+            //подготовка
+            Calculation clac = new Calculation();
+            int typeProduct = -3;
+            int typeMaterial = 1;
+            int count = 15;
+            float w = 20f;
+            float h = 45f;
+            int expected = -1;
+            //test
+            var actual = clac.GetQuantityForProduct(typeProduct, typeMaterial, count, w, h);
+            //проверка
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
